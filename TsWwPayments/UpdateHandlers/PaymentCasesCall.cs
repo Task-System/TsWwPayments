1910a67d16f3383f170385e33a774811d96eb568
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2022-01-13 17:08:07 +0330

    Fixes on extensions

--- TsWwPayments/UpdateHandlers/PaymentCasesCall.cs
+++ TsWwPayments/UpdateHandlers/PaymentCasesCall.cs
@@ -8,7 +8,9 @@ namespace TsWwPayments.UpdateHandlers
     {
         protected override async Task HandleUpdate(SimpleContext<CallbackQuery> ctx)
         {
-            await ctx.Answer("Tested!", true);
+            await ctx.If("pay_cases_part1", x => x.Answer("On part 1"))
+                .ElseIf("pay_case_part2", x => x.Answer("On part 2"))
+                .Else(x => x.Answer("On part Unknown"));
         }
     }
 }
